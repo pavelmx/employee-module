c44aab301d58e1cb963a1ec4e7819d9194c21335
oadmin <mx-store@inbox.ru>
2019-06-07 17:16:35 +0300

    add Q classes

---
+++ src/main/java/entity/QAbstractEntity.java
@@ -0,0 +1,41 @@
+package entity;
+
+import com.innowise.employeemodule.entity.AbstractEntity;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.PathMetadata;
+import com.querydsl.core.types.dsl.DateTimePath;
+import com.querydsl.core.types.dsl.EntityPathBase;
+
+import javax.annotation.Generated;
+
+import static com.querydsl.core.types.PathMetadataFactory.forVariable;
+
+
+/**
+ * QAbstractEntity is a Querydsl query type for AbstractEntity
+ */
+@Generated("com.querydsl.codegen.SupertypeSerializer")
+public class QAbstractEntity extends EntityPathBase<AbstractEntity> {
+
+    private static final long serialVersionUID = -2125310381L;
+
+    public static final QAbstractEntity abstractEntity = new QAbstractEntity("abstractEntity");
+
+    public final DateTimePath<java.time.LocalDateTime> created = createDateTime("created", java.time.LocalDateTime.class);
+
+    public final DateTimePath<java.time.LocalDateTime> updated = createDateTime("updated", java.time.LocalDateTime.class);
+
+    public QAbstractEntity(String variable) {
+        super(AbstractEntity.class, forVariable(variable));
+    }
+
+    public QAbstractEntity(Path<? extends AbstractEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QAbstractEntity(PathMetadata metadata) {
+        super(AbstractEntity.class, metadata);
+    }
+
+}
+
---
+++ src/main/java/entity/QPosition.java
@@ -0,0 +1,50 @@
+package entity;
+
+import com.innowise.employeemodule.entity.Position;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.PathMetadata;
+import com.querydsl.core.types.dsl.*;
+
+import javax.annotation.Generated;
+
+import static com.querydsl.core.types.PathMetadataFactory.forVariable;
+
+
+/**
+ * QPosition is a Querydsl query type for Position
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QPosition extends EntityPathBase<Position> {
+
+    private static final long serialVersionUID = -988861609L;
+
+    public static final QPosition position = new QPosition("position1");
+
+    public final QAbstractEntity _super = new QAbstractEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> created = _super.created;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isActive = createBoolean("isActive");
+
+    public final StringPath name = createString("name");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updated = _super.updated;
+
+    public QPosition(String variable) {
+        super(Position.class, forVariable(variable));
+    }
+
+    public QPosition(Path<? extends Position> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPosition(PathMetadata metadata) {
+        super(Position.class, metadata);
+    }
+
+}
+
